5c7efe48d707a6e169667a602c9c149f06a45f71
Ronny Pereira <ronny.pereira@ipdec.org>
2023-02-09 11:23:24 -0400

    Api concluída

--- cadastrousuarios/src/usuario/usuario.controller.ts
+++ cadastrousuarios/src/usuario/usuario.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Post, Body, Get, Param, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, HttpStatus, NotFoundException } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { Usuario } from './usuario.entity';
+import { NestResponse } from '../core/http/nest-response';
+import { NestResponseBuilder } from '../core/http/nest-response-builder';
 
 @Controller('users')
 export class UsuarioController {
@@ -10,17 +12,25 @@ export class UsuarioController {
     @Get(':nomeDeUsuario')
     public buscaPorNomeDeUsuario(@Param('nomeDeUsuario') nomeDeUsuario: string): Usuario {
         const usuarioEncontrado = this.usuarioService.buscaPorNomeDeUsuario(nomeDeUsuario);
-        
+
+        if(!usuarioEncontrado){
+            throw new NotFoundException({
+                statusCode: HttpStatus.NOT_FOUND,
+                message: 'Usuário não encontrado'
+            });
+        }
         return usuarioEncontrado;
     }
 
     @Post()
-    public cria(@Body() usuario: Usuario, @Res() res): Usuario {
+    public cria(@Body() usuario: Usuario): NestResponse {
         const usuarioCriado = this.usuarioService.cria(usuario);
-        res.status(HttpStatus.CREATED)
-            .location(`/users/${usuarioCriado.nomeDeUsuario}`)
-            .json(usuarioCriado);
-
-        return usuarioCriado;
+        return new NestResponseBuilder()
+            .comStatus(HttpStatus.CREATED)
+            .comHeaders({
+                'Location':`/users/${usuarioCriado.nomeDeUsuario}`
+            })
+            .comBody(usuarioCriado)
+            .build();
     }
 }
